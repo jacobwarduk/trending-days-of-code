33456c2a2ffca55d88adeb2ab0c13bafdc320a68
Jacob Ward <jacob@candyboxmedia.com>
2017-07-30 07:19:05 +0100

    Add initial API request

--- js/Trending.jsx
+++ js/Trending.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import axios from 'axios';
+import {apiUrl, baseRepo} from '../consts';
 
-class Landing extends React.Component {
+class Trending extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			repos: []
+		};
+	}
+
+	componentDidMount() {
+		this.getForkedRepos(1);
+	}
+
+	getForkedRepos(page) {
+		axios.get(`${apiUrl}/repos/${baseRepo}/forks?page=${page}`)
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Its alive...!</h1>
+				<Button bsStyle="success" bsSize="small">Test Button</Button>
 			</div>
 		);
 	}
 }
 
-export default Landing;
+export default Trending;
