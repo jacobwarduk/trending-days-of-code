7162fb0ce63f25468ca2de4bb0e6094dd4022634
Jacob Ward <jacob@candyboxmedia.com>
2017-08-02 15:35:31 +0100

    Add dynamic routing to components

--- js/App.jsx
+++ js/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import FourOhFour from './404';
 import Trending from './Trending';
 import Header from './Header';
+import Log from './Log';
 
 class App extends React.Component {
 	render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
 					<div className="app">
 						<Switch>
 							<Route exact path="/" component={ Trending } />
+							<Route path="/log/:id" component={ Log } />
 							<Route component={ FourOhFour } />
 						</Switch>
 					</div>
